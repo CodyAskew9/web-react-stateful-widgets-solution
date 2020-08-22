b3064e966a1877d209f7c7b5e304247fb72d8d51
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-22 17:53:47 +0200

    programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -27,8 +27,8 @@ const listOfAwesome = [
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-  const [programmers] = useState(listOfAwesome)
-  const [featured, setFeatured] = useState(null)
+  const [programmers] = useState(listOfAwesome);
+  const [featured, setFeatured] = useState(null);
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -36,7 +36,7 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    return programmers.find(prog => prog.id === featured).name
+    return programmers.find(prog => prog.id === featured).name;
   };
 
   const style = {
@@ -55,7 +55,7 @@ export default function Programmers() {
           we could never add or edit programmers in the future. The list would be a static thing." */
           programmers.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { setFeatured(dev.id) /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { setFeatured(dev.id); /* in here set the featured id to be dev.id */ }}>Feature</button>
             </div>
           )
         }
